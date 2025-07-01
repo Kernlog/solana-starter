1874c2fb3138c403c19ed57f3a70184063c95b0a
Kernlog <0xkernlog@gmail.com>
2025-07-01 01:13:42 -0400

    chore: upload nft image & metadata, then mint

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,4 +1,4 @@
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbin3-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -10,7 +10,7 @@ const umi = createUmi('https://api.devnet.solana.com');
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
+umi.use(irysUploader({ address: "https://devnet.irys.xyz/" }));
 umi.use(signerIdentity(signer));
 
 (async () => {
@@ -19,10 +19,17 @@ umi.use(signerIdentity(signer));
         //2. Convert image to generic file.
         //3. Upload image
 
-        // const image = ???
+        // Using the image.png
+        const image = await readFile("../image.png");
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        // Convert to generic file
+        const genericFile = createGenericFile(image, "image.png", {
+            contentType: "image/png"
+        });
+
+        // Upload image
+        const [myUri] = await umi.uploader.upload([genericFile]);
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbin3-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,28 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    // Metadata URI from nft_metadata.ts
+    const metadataUri = "https://gateway.irys.xyz/CfeQ9suRBhyt8VxtRUqtUdbBWCDy2tR2GiDk5xk1BWT1";
+
+    let tx = createNft(umi, {
+        mint: mint,
+        name: "Turbin3 Rug",
+        symbol: "T3RUG",
+        uri: metadataUri,
+        sellerFeeBasisPoints: percentAmount(5), // 5% royalty
+        creators: [
+            {
+                address: myKeypairSigner.publicKey,
+                verified: true,
+                share: 100
+            }
+        ]
+    });
+
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Successfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
